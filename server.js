f7aaca0748a66114bca372d3457e1b535a7ca509
Nikhil Shenoy <nikhilshenoy17@gmail.com>
2017-07-30 18:27:01 -0400

    infrastructure

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+// Create our app
+var app = express();
+
+// Webpack dumps the bundle.js in the 'public' directory
+// The index.html file there imports the bundle.js
+// Express automatically looks for and serves an index.html on startup
+app.use(express.static('public'));
+
+app.listen(3000, function() {
+	console.log('Express server is running on port 3000!');
+});
