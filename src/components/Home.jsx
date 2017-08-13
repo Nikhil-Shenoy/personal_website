1f6e9db6312d46caa5764826e1aeacaaacce21c6
Nikhil Shenoy <nikhilshenoy17@gmail.com>
2017-08-13 11:27:15 -0400

    Basic Home Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,30 +7,37 @@ import styled from 'styled-components';
 
 // @todo: Find out how to make this a styled component
 var pro_pic_styles = {
-	'width': '150px',
-	'height': '150px',
-	'border-radius': '8px',
-	'align': 'center'
+	'width': '450px',
+	'height': '450px',
+	'borderRadius': '8px',
+	'margin': '0 auto',
+	// 'padding': '20px'
 }
 
 const Title = styled.h1`
 	text-align: center;
-	color: palevioletred;
+	padding: 20px;
 `;
 
-				// <h1>This is the Home Page</h1>
+
+// Styling is a pain in the ass
+
 var Home = React.createClass({
 	render: function() {
 		return (
 			<div>
-				<Title>This is the Home Page</Title>
-				<img style={pro_pic_styles} src={ProfilePic}/>
-				<p>This is me. I am just putting some placeholder text here. </p>
-				<p>Eventually this should contain an overview of the site</p>
+				<Title>Nikhil Shenoy</Title>
+				<div style={{"textAlign": "center" }}>
+					<img style={pro_pic_styles} src={ProfilePic}/>
+				</div>
+				<p style={{ "textAlign": "center", "padding": "30px" }}>
+					I am an engineer, and I'm always looking to learn new things. 
+				</p>
 			</div>
 		)
 	}
 });
+				// <p style={{ p_styles }}>Eventually this should contain an overview of the site</p>
 				// <TopBar/>
 
 module.exports = Home;
