6187183deb27909a2307c8c333574ba3897cc0c0
Nikhil Shenoy <nikhilshenoy17@gmail.com>
2017-08-13 13:53:05 -0400

    Finished social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,24 +4,18 @@ import FaLinkedinSquare from 'react-icons/lib/fa/linkedin-square';
 import FaGithub from 'react-icons/lib/fa/github'
 import MdEmail from 'react-icons/lib/md/email'
 
-
+var icon_style = {
+	'padding': '20px',
+}
 
 var Footer = React.createClass({
 	render: function() {
 		return (
-			<div>
-				<a href="www.facebook.com">
-					<FaFacebookOfficial size="36"/>
-				</a>
-				<a href="www.linkedin.com">
-					<FaLinkedinSquare/>
-				</a>
-				<a href="www.github.com">
-					<FaGithub/>
-				</a>
-				<a href="www.gmail.com">
-					<MdEmail/>
-				</a>
+			<div style={{ "textAlign": "center" }}>
+				<a style={ icon_style } href="https://www.facebook.com/nikhil.shenoy.37" target="_blank"><FaFacebookOfficial size="36"/></a>
+				<a style={ icon_style } href="https://www.linkedin.com/in/nikhil-shenoy-02059257/" target="_blank"><FaLinkedinSquare size="36"/></a>
+				<a style={ icon_style } href="https://github.com/Nikhil-Shenoy" target="_blank"><FaGithub size="36"/></a>
+				<a style={ icon_style } href="mailto:nikhilshenoy17@gmail.com"><MdEmail size="36"/></a>
 			</div>
 
 		)
