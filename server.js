834c6e4194b9725889f541e1811a6ebe7e692389
AD005\z003pudj <nikhil.shenoy@siemens.com>
2018-03-20 07:01:37 -0400

    space

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 // Webpack dumps the bundle.js in the 'public' directory
 // The index.html file there imports the bundle.js
 // Express automatically looks for and serves an index.html on startup
+
 app.use(express.static('public'));
 
 app.listen(3000, function() {
